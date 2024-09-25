9e642726164e1ede182863097103519949587292
Vitor Calejuri <vitor.cajuleri@gmail.com>
2024-09-25 16:46:43 -0300

    Added update hook

--- assets/js/phoenix_live_react.js
+++ assets/js/phoenix_live_react.js
@@ -11,7 +11,10 @@ const loadReact = async () => {
   }
 }
 
+let reactRoots = {};
+
 const render = async function (el, target, componentClass, additionalProps = {}, previousProps = {}) {
+  const _root = {}
   const { React, ReactDOMClient } = await loadReact();
   let props = el.dataset.liveReactProps ? JSON.parse(el.dataset.liveReactProps) : {};
   if (el.dataset.liveReactMerge) {
@@ -19,9 +22,14 @@ const render = async function (el, target, componentClass, additionalProps = {},
   } else {
     props = { ...props, ...additionalProps }
   }
+
+  if (!reactRoots[el.id]) {
+    console.info("start react root", el.id)
+    reactRoots[el.id] = ReactDOMClient.createRoot(target);
+  }
+
   const reactElement = React.createElement(componentClass, props);
-  const root = ReactDOMClient.createRoot(target);
-  root.render(reactElement)
+  reactRoots[el.id].render(reactElement)
   return props;
 }
 
@@ -41,26 +49,25 @@ const initLiveReactElement = async function (el, additionalProps) {
 
 const LiveReact = {
   async mounted() {
-    // const { el } = this;
-    // const pushEvent = this.pushEvent.bind(this);
-    // const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
-    // const handleEvent = this.handleEvent && this.handleEvent.bind(this);
-    const { target, componentClass } = await initLiveReactElement(this.el);
-    //{ pushEvent });
-    // debugger;
-    // const props = render(el, target, componentClass, { pushEvent, pushEventTo, handleEvent });
-    // if (el.dataset.liveReactMerge) this.props = props
-    // Object.assign(this, { target, componentClass });
+    const { el } = this;
+    const pushEvent = this.pushEvent.bind(this);
+    const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
+    const handleEvent = this.handleEvent && this.handleEvent.bind(this);
+    const { target, componentClass } = await initLiveReactElement(this.el, { pushEvent, pushEventTo, handleEvent });
+    const props = await render(el, target, componentClass, { pushEvent, pushEventTo, handleEvent });
+    if (el.dataset.liveReactMerge) this.props = props
+    Object.assign(this, { target, componentClass });
   },
 
-  updated() {
-    // const { el, target, componentClass } = this;
-    // const pushEvent = this.pushEvent.bind(this);
-    // const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
-    // const handleEvent = this.handleEvent;
-    // const previousProps = this.props;
-    // const props = render(el, target, componentClass, { pushEvent, pushEventTo }, previousProps);
-    // if (el.dataset.liveReactMerge) this.props = props
+  async updated() {
+    const { el, target, componentClass } = this;
+    const pushEvent = this.pushEvent.bind(this);
+    const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
+    const handleEvent = this.handleEvent;
+    const previousProps = this.props;
+
+    const props = await render(el, target, componentClass, { pushEvent, pushEventTo }, previousProps);
+    if (el.dataset.liveReactMerge) this.props = props
   },
 
   destroyed() {
